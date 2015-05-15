28c6ba5477c8e2dd215ff6c8fbc3c8fdeb90fd1c
maxgurewitz <maxthegeek1@gmail.com>
2015-05-14 22:30:54 -0700

    restructure client app and add sass compilation

--- gulpfile.js
+++ gulpfile.js
@@ -3,39 +3,52 @@ var webpack = require('webpack');
 var nodemon = require('gulp-nodemon');
 var gutil = require('gulp-util');
 var WebpackDevServer = require('webpack-dev-server');
+var sass = require('gulp-sass');
 
 gulp.task('webpack', function (done) {
 
   var transpiler = webpack({ 
-      module: {
-        loaders: [
-          { test: /\.jsx$/, loader: 'jsx-loader' }
-        ],
-      },
-      entry: {
-        home: __dirname + '/src/client/index.jsx'
-      },
-      output: {
-        path: __dirname + '/dist/js/',
-        filename: 'bundle.js'
-      },
-      resolveLoader: {
-        root: __dirname + '/node_modules'
-      },
-      plugins: [
-        new webpack.ProvidePlugin({
-          React: 'react'
-        })
+    module: {
+      loaders: [
+      { test: /\.jsx$/, loader: 'jsx-loader' }
+      ],
+    },
+    entry: {
+      home: __dirname + '/src/client/js/index.jsx'
+    },
+    output: {
+      path: __dirname + '/dist/js/',
+      filename: 'bundle.js'
+    },
+    resolveLoader: {
+      root: __dirname + '/node_modules'
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        React: 'react'
+      })
+    ]
+  });
+
+  new WebpackDevServer(transpiler, {
+    publicPath: __dirname + '/dist/js/',
+  }).listen(8080, "localhost", function (err) {
+    if(err) { throw new gutil.PluginError("webpack-dev-server", err) };
+
+    gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+  });
+});
+
+gulp.task('sass', function () {
+  gulp.src(__dirname + '/src/client/css/**/*.scss')
+    .pipe(sass({ 
+      errLogToConsole: true,
+      style: 'compressed',
+      includePaths: [
+        __dirname + '/node_modules/bootstrap-sass/assets/stylesheets/'
       ]
-    });
-
-    new WebpackDevServer(transpiler, {
-      publicPath: __dirname + '/dist/js/',
-    }).listen(8080, "localhost", function (err) {
-        if(err) { throw new gutil.PluginError("webpack-dev-server", err) };
-
-        gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
-    });
+    }))
+  .pipe(gulp.dest(__dirname + '/dist/css/'));
 });
 
 gulp.task('start', function () {
@@ -46,4 +59,4 @@ gulp.task('start', function () {
   });
 });
 
-gulp.task('default', ['start', 'webpack']);
+gulp.task('default', ['start', 'webpack', 'sass']);
