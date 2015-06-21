1e6db93204226cc5e8a66e61d2f33d622a6f70fb
Max Gurewitz <mgurewitz@MacBook-Pro.local>
2015-06-21 02:39:25 -0700

    basic hapi structure

--- src/app/controllers/users.js
+++
@@ -1,7 +0,0 @@
-module.exports.create = function(req, res) { 
-  res.sendStatus(200);
-};
-
-module.exports.get = function(req, res) {
-  res.json({ foo: 'baz' });
-};
--- src/app/index.js
+++ src/app/index.js
@@ -1,21 +1,2 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var logger = require('morgan');
-var ejs = require('ejs');
-
-GLOBAL.__BASE = __dirname;
-
-var routes = require('./routes');
-
-
-app.use(logger('combined'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.engine('ejs', ejs.renderFile);
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(express.static('./dist', { index: false }));
-app.use(routes);
-
-module.exports = app;
+var requireDirectory = require('require-directory');
+module.exports = requireDirectory(module);
